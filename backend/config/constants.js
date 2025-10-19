49a5e3990720af610304d9beca0d9ebe38362ee3
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-10-20 03:08:58 +0600

    Update constants.js

---
+++ backend/config/constants.js
@@ -0,0 +1,9 @@
+export const ORDER_STATUS = {
+  PENDING: "Pending",
+  CONFIRMED: "Confirmed",
+  PREPARING: "Preparing",
+  PICKED_UP: "PickedUp",
+  ON_THE_WAY: "OnTheWay",
+  DELIVERED: "Delivered",
+  CANCELLED: "Cancelled"
+};
