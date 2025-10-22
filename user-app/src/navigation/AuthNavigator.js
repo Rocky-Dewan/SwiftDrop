f41f2b3e583dcf70be89c7269cbfbe3a5493da26
Rocky Dewan <dewanrocky250@gmail.com>
2025-10-22 08:23:59 +0600

    Update AuthNavigator.js
    Co-Authored-By: Litis Trion <sojibali0102@gmail.com>

---
+++ user-app/src/navigation/AuthNavigator.js
@@ -0,0 +1,16 @@
+// optional if you want separate auth flow
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+}
