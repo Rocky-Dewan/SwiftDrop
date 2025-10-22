2069d59f20023fbabe6bb47999dfa1c44a5ea086
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-10-22 07:31:22 +0600

    Update userService.js

--- user-app/src/services/userService.js
+++ user-app/src/services/userService.js
@@ -1,16 +1,1 @@
-import api from './api';
 
-export const loginUser = async (phone, password) => {
-  const res = await api.post('/users/login', { phone, password });
-  return res.data;
-};
-
-export const registerUser = async (payload) => {
-  const res = await api.post('/users/register', payload);
-  return res.data;
-};
-
-export const fetchProfile = async () => {
-  const res = await api.get('/users/profile');
-  return res.data;
-};
